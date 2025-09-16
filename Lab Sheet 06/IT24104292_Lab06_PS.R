c10de56e248b946218992394e60eac37f26f4add
IT24104292 <IT24104292@my.sliit.lk>
2025-09-16 12:02:39 +0530

    IT24104292 - lab6

---
+++ Lab Sheet 06/IT24104292_Lab06_PS.R
@@ -0,0 +1,13 @@
+setwd("C:\\Users\\USER\\OneDrive\\Desktop\\IT24102440")
+getwd()
+
+#1)
+#i. Binomial distribution
+#ii.
+1-pbinom(46,50,0.85,lower.tail=TRUE)
+
+#2)
+#i. Number of calls per hour
+#ii. Possion distribution.
+#iii. 
+dpois(15,12)
